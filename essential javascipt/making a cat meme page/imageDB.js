aedb04b5a92346be3f7b2b4d53e9975fe02789c8
Aryan Das <aryandas079@outlook.com>
2026-05-29 01:05:20 +0530

    second assignment for essential JavaScript
    this is the second assignment of making a cat model in JavaScript

---
+++ essential javascipt/making a cat meme page/imageDB.js
@@ -0,0 +1,64 @@
+export const imageDatabase = {
+    angry: [
+        "angry.gif",
+        "angry.jpeg",
+        "angry2.gif",
+        "angry2.jpeg",
+        "angry3.jpeg"
+    ],
+
+    confused: [
+        "confused.gif",
+        "confused.jpeg",
+        "confused2.gif"
+    ],
+
+    dominant: [
+        "dominant.gif",
+        "dominant.jpeg"
+    ],
+
+    happy: [
+        "happy.gif",
+        "happy.jpeg"
+    ],
+
+    hungry: [
+        "hungry.gif",
+        "hungry.jpeg",
+        "hungry1.jpeg",
+        "hungry2.gif"
+    ],
+
+    insomnia: [
+        "insomnia.gif",
+        "insomnia.jpeg",
+        "insomnia1.jpeg",
+        "insomnia2.gif"
+    ],
+
+    lazy: [
+        "lazy.gif",
+        "lazy.jpeg"
+    ],
+
+    nervous: [
+        "nervous.gif",
+        "nervous.jpeg",
+        "nervous2.gif"
+    ],
+
+    relaxed: [
+        "relaxed2.gif"
+    ],
+
+    sad: [
+        "sad.gif",
+        "sad.jpeg",
+        "sad1.jpeg"
+    ],
+
+    scared:[
+        "scared.jpeg"
+    ]
+}
